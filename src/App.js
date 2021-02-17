f60e4542c46e6ee521f1616e5abbd5b80bdf538c
its4zahoor <its4zahoor@gmail.com>
2021-02-17 20:25:37 +0500

    imported and consumed bit collection component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ImageScroll from "./imageScroll";
 import TextShadow from "./text/textShadow";
 import Lists from "./lists";
 import useTimer from "./useTimer";
+import ProductList from "@bit/its4zahoor.bit-react-tutorial.product-list";
 
 const images = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
 function App() {
@@ -12,6 +13,9 @@ function App() {
   const time2 = useTimer(startDateTime2);
   return (
     <div>
+      <div className="App">
+        <ProductList />
+      </div>
       <div>
         {time} <br /> {time2}
       </div>
